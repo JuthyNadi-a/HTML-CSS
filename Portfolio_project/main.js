e32e0e026c480b7a46748bdc7405be5b32d0239c
JuthyNadi-a <juthynadia@gmail.com>
2022-10-06 11:06:25 +0300

    get to top function added

--- Portfolio_project/main.js
+++ Portfolio_project/main.js
@@ -1,13 +1,22 @@
 const header = document.querySelector('header');
+let topButton = document.querySelector('#topBtn');
 
 window.onscroll = function () {
     scrollFunction();
-};
-const scrollFunction = () => {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
-    {
-    header.classList.add('active');
+  };
+  
+  const scrollFunction = () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.classList.add('active');
+      topButton.style.display = 'block';
     } else {
-        header.classList.remove('active')
+      header.classList.remove('active');
+      topButton.style.display = 'none';
     }
-}
+  };
+  
+const getToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+topButton.addEventListener('click', getToTop);
